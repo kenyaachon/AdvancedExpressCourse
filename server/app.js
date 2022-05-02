73b068cc0997b541c89b39ba1c74714bb844145e
mosesmbugua <swahili1963@gmail.com>
2022-05-02 14:20:16 -0700

    Initial Structure

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const createError = require("http-errors");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const SpeakerService = require("./services/SpeakerService");
+const FeedbackService = require("./services/FeedbackService");
+
+module.exports = (config) => {};
