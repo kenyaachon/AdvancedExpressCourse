74680c34a9b5362c0e741401eedf0fdc63d1a08c
mosesmbugua <swahili1963@gmail.com>
2022-05-02 22:25:11 -0700

    Add New Header

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = () => {
-  router.get("/registration", (req, res) =>
-    res.render("users/registration", { success: req.query.success })
-  );
+//Require the index file
+const speakersRoute = require("./speakers");
 
-  router.get("/account", (req, res) => {
-    res.render("users/account", { user: req.user });
+module.exports = (params) => {
+  // Destructuring assignment
+  const { speakers } = params;
+
+  //Now let's define the index route and mount it on slash
+  router.get("/", async (req, res) => {
+    const speakerslist = await speakers.getListShort();
+    const artwork = await speakers.getAllArtwork();
+    return res.render("index", { page: "Home", speakerslist, artwork });
   });
+
+  //And mount it to the path speakers
+  router.use("/speakers", speakersRoute(params));
 
   return router;
 };
--- server/routes/speakers/index.js
+++ server/routes/speakers/index.js
@@ -15,5 +15,19 @@ module.exports = (params) => {
     });
   });
 
+  router.get("/:name", async (req, res, next) => {
+    const speaker = await speakers.getSpeaker(req.params.name);
+    //This will fall through and create a NOT FOUND
+    if (!speaker) return next();
+    const artwork = await speakers.getArtworkForSpeaker(req.params.name);
+    console.log(speaker);
+    console.log(artwork);
+    return res.render("speakers/detail", {
+      page: req.params.name,
+      artwork,
+      speaker,
+    });
+  });
+
   return router;
 };
