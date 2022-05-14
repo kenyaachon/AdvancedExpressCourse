68e377dda3e35c42f806c47bcb93901056798c0c
mosesmbugua <swahili1963@gmail.com>
2022-05-13 19:44:19 -0700

    Add User Registration Route

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const UserModel = require("../../models/UserModel");
+
 const router = express.Router();
 
 module.exports = () => {
@@ -8,6 +10,23 @@ module.exports = () => {
       success: req.query.success,
     })
   );
+
+  router.post("/registration", async (req, res, next) => {
+    try {
+      const user = new UserModel({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      //save() is a built-in function from mongo
+      const savedUser = await user.save();
+      if (savedUser) return res.redirect("/users/registration?success=true");
+      return next(new Error("Failed to save user for unknown reasons"));
+    } catch (err) {
+      return next(err);
+    }
+  });
 
   router.get("/account", (req, res) =>
     res.render("users/account", {
